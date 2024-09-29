04d7ffd798176ed62d9de42d3621ff3bc0a71f95
lesca <5976219+lesca@users.noreply.github.com>
2024-09-29 14:12:37 +0800

    update

--- src/settings.js
+++ src/settings.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Define default prompts
   const defaultPrompts = {
     system: "You are a helpful assistant that provides English word explanations in Simplified Chinese. You output in markdown lists under highlighted headers without ``` block.",
-    summarize: "1. 列出文中出现的人物或组织（翻译、背景信息、文中关联）。\n2. 列出文中出现的地点或场所（翻译、背景信息、文中关联）。\n3. 根据时间、地点、人物、起因、经过、结果等，列出文中的事件，未提及的标注“不详”。\n4. 整理出文中的未来预期，列出人物、原因、预期等，未提及的标注“不详”。\n5. 列出文中所有评论或观点，不要遗漏，并列出人物、人物身份、人物观点等，未中未提及的标注“不详”。\n6. 最后根据以上信息补充并总结全文内容。\n\n全文信息如下：\n{{Text}}",
+    summarize: "1. 列出文中出现的人物或组织（翻译、背景信息、文中关联）。\n2. 列出文中出现的地点或场所（翻译、背景信息、文中关联）。\n3. 根据时间、地点、人物、起因、经过、结果等，列出文中的事件，未提及的标注“不详”。\n4. 整理出文中的未来预期，列出人物、原因、预期等，未提及的标注“不详”。\n5. 列出文中所有评论或观点，不要遗漏，并列出人物、人物身份、人物观点等，未提及的标注“不详”。\n6. 最后根据以上信息补充并总结全文内容。\n\n全文信息如下：\n{{Text}}",
     translate: "先以列表形式解释{{Text}}中的词组（缩进形式列出国际音标、翻译、原文含义、常用短语），最后根据上下文给出{{Text}}的中文翻译。 \n\n注意，词组不要太长，原文含义可以拓展背景知识，常用短语需要中英文对照。",
     explain: "先解释{{Text}}常见的含义，然后给出{{Text}}在上下文中的含义，最后给出{{Text}}所在句子的中文翻译。\n\n上下文：\n{{Paragraph}} ",
     custom: "先解释{{Text}}常见的含义，然后推理{{Text}}在上下文中的含义，并给出推理过程中所用到的原文信息，最后给出{{Text}}所在句子的中文翻译。\n\n上下文：\n{{Paragraph}} \n\n全文：\n{{Page}}"
